08347dbc4868487935bf92d16e5e00e4d2d46b4d
maher-alhomsy <maheralhomsy2002@gmail.com>
2025-02-17 20:20:58 +0300

    create user controller

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,14 @@
+import { User } from '../model/user.model.js';
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const currentUserId = req.auth.userId;
+    const users = await User.find({ clerkId: { $ne: currentUserId } });
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.log('Error in get all users');
+
+    next(error);
+  }
+};
